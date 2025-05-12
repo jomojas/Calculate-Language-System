5209185a70a7b74ee273b600b17e8aea8b1691e6
Jomo <jomojassen@gmail.com>
2025-05-12 16:51:18 +0800

    String and Bool Can compare(== !=)

--- src/SemanticAnalysis/Interpret.java
+++ src/SemanticAnalysis/Interpret.java
@@ -419,7 +419,20 @@ public class Interpret {
 				} else {
 					return new BoolRunResult(false);
 				}
-			} else {
+			} else if(left instanceof StringRunResult strRes1 && right instanceof StringRunResult strRes2) { 
+				if(strRes1.value.equals(strRes2.value)) {
+					return new BoolRunResult(false);
+				} else {
+					return new BoolRunResult(true);
+				}
+			} else if(left instanceof BoolRunResult boolRes1 && right instanceof BoolRunResult boolRes2) {
+				if(boolRes1.value == boolRes2.value) {
+					return new BoolRunResult(false);
+				} else {
+					return new BoolRunResult(true);
+				}
+			}
+			else {
 				throw new MyException("Unsupported Type For Comparison: " + left.getResultType());
 			}
 //			break;
